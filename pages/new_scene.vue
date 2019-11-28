c8e2383649ba716939dab2b539218516ec614512
akakou <akakou571@gmail.com>
2019-11-28 18:27:12 +0900

    投稿ページを追加

---
+++ pages/new_scene.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>text content</p>
+    <textarea v-model="content"></textarea>
+    <br />
+    <button type="button" value="submit" v-on:click="submit()">ログイン</button>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+// import firebase from "app";
+
+// const app = firebase.init();
+export default {
+  methods: {
+    submit() {
+      const scene = new this.$Scene();
+      this.$SceneRepository.store(scene);
+      location.href = "/";
+    }
+  },
+  mounted() {},
+  data() {
+    return {
+      content: ""
+    };
+  }
+};
+</script>
+
+<style></style>
