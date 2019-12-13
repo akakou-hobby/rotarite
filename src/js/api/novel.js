e283f974bbd849606fde08ab4d5682d6edad9e0d
akakou <akakou571@gmail.com>
2019-12-13 20:57:08 +0900

    novelの扱いを修正

--- src/js/api/novel.js
+++ src/js/api/novel.js
@@ -1,32 +1,30 @@
 class Novel {
-  constructor(data) {
-    this.data = data;
+  constructor({ id = null, title = null, summary = null, root = null }) {
+    this.id = id;
+    this.title = title;
+    this.summary = summary;
+    this.root = root;
   }
 }
 
 class NovelRepository {
   constructor() {}
-  create({ title = null, summary = null, rootContent = null }) {
+  create({ title = null, summary = null, root = null }) {
     const scaneRepo = new SceneRepository();
 
     const date = new Date();
     const now = date.getTime();
 
-    const root = scaneRepo.create({
-      content: rootContent,
-      novel: {
-        data: {
-          id: now
-        }
-      },
-      prev: null
-    });
+    root.id = now;
+    root.prevId = null;
+
+    scaneRepo.store(root);
 
     const novel = new Novel({
       id: now,
       title: title,
       summary: summary,
-      root: root.data.id
+      root: root.id
     });
 
     this.store(novel);
@@ -34,17 +32,13 @@ class NovelRepository {
   }
   store(novel) {
     const db = firebase.firestore();
-
-    const date = new Date();
-    const now = date.getTime();
-
     const currentUser = getCurrentUser();
 
     db.collection("users")
       .doc(currentUser.uid)
       .collection("novel")
-      .doc(novel.data.id)
-      .set(novel.data);
+      .doc(novel.id)
+      .set(novel);
   }
 
   async findById(novelId) {
