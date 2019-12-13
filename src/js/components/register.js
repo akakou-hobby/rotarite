69a0a0b778619df14c034a79b82589b48b171684
akakou <akakou571@gmail.com>
2019-12-13 16:53:14 +0900

    入力を受け取った

--- src/js/components/register.js
+++ src/js/components/register.js
@@ -1,15 +1,41 @@
 class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin(e) {
+    console.log(e);
+    alert("email: " + this.state.email);
+    alert("password: " + this.state.password);
+  }
+
   render() {
     return (
       <div>
         <h1>登録</h1>
         <p>メールアドレス</p>
-        <input type="email" placeholder="taro@example.com" />
+        <input
+          type="email"
+          value={this.state.email}
+          placeholder="taro@example.com"
+          onChange={e => this.setState({ email: e.target.value })}
+        />
         <br />
         <p>パスワード</p>
-        <input type="password" placeholder="********" />
+        <input
+          type="password"
+          value={this.state.password}
+          placeholder="********"
+          onChange={e => this.setState({ password: e.target.value })}
+        />
         <br />
-        <button type="button" value="submit" onClick={e => {}}>
+        <button type="button" value="submit" onClick={this.handleLogin}>
           新規登録
         </button>
         <button
