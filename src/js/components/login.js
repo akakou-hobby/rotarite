9b948ccebe735c90d397f2b9de5aa5d8ad965296
akakou <akakou571@gmail.com>
2019-12-13 17:20:37 +0900

    ログインする処理を書いた

--- src/js/components/login.js
+++ src/js/components/login.js
@@ -1,15 +1,52 @@
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin(e) {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        location.href = "/";
+      }
+    });
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+      });
+  }
+
   render() {
     return (
       <div>
         <h1>ログイン</h1>
         <p>メールアドレス</p>
-        <input type="email" placeholder="taro@example.com" />
+        <input
+          type="email"
+          value={this.state.email}
+          placeholder="taro@example.com"
+          onChange={e => this.setState({ email: e.target.value })}
+        />
         <br />
         <p>パスワード</p>
-        <input type="password" placeholder="********" />
+        <input
+          type="password"
+          value={this.state.password}
+          placeholder="********"
+          onChange={e => this.setState({ password: e.target.value })}
+        />
         <br />
-        <button type="button" value="submit" onClick={e => {}}>
+        <button type="button" value="submit" onClick={this.handleLogin}>
           ログイン
         </button>
       </div>
