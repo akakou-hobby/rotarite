06a3bf1681c7161ae01202ade19a0ee1ebd874da
akakou <akakou571@gmail.com>
2019-12-23 17:06:17 +0900

    FirestoreObject用のリポジトリを追加

--- js/api/common.js
+++ js/api/common.js
@@ -5,3 +5,47 @@ class FirestoreObject {
     throw new Error("Not Implemented Yet");
   }
 }
+
+class FirestoreObjectRepository {
+  constructor(firestoreObjectClass) {
+    this.firestoreObjectClass = firestoreObjectClass;
+    this.repositoryName = firestoreObjectClass.name.toLowerCase();
+  }
+
+  create() {
+    throw new Error("Not Implemented Yet");
+  }
+
+  store(collectionObject) {
+    const db = firebase.firestore();
+    const uid = currentUser().uid;
+
+    db.collection("users")
+      .doc(uid)
+      .collection(this.repositoryName)
+      .doc(collectionObject.id.toString())
+      .set(collectionObject.data())
+      .then(() => {
+        console.log(`${collectionObject.id} successfully written!`);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  async findById(collectionObjectId) {
+    const db = firebase.firestore();
+
+    const snapshots = await db
+      .collectionGroup(this.repositoryName)
+      .where("id", "==", collectionObjectId)
+      .get();
+
+    var data = null;
+    snapshots.forEach(doc => {
+      data = doc.data();
+    });
+
+    return new this.firestoreObjectClass(data);
+  }
+}
