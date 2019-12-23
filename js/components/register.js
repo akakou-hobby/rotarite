dce62dedc5e9403eb02a5bfa157cb98a4ccf4992
akakou <akakou571@gmail.com>
2019-12-23 22:35:20 +0900

    新規登録画面をいけいけにした

--- js/components/register.js
+++ js/components/register.js
@@ -28,33 +28,51 @@ class Register extends React.Component {
   render() {
     return (
       <div>
-        <h1>登録</h1>
-        <p>メールアドレス</p>
-        <input
-          type="email"
-          value={this.state.email}
-          placeholder="taro@example.com"
-          onChange={e => this.setState({ email: e.target.value })}
-        />
+        <h1 className="title">登録</h1>
+        <div className="field">
+          <label className="label">メールアドレス</label>
+          <div className="control">
+            <input
+              className="input"
+              type="email"
+              placeholder="taro@example.com"
+              value={this.state.email}
+              onChange={e => this.setState({ email: e.target.value })}
+            />
+          </div>
+        </div>
+
+        <div className="field">
+          <label className="label">パスワード</label>
+          <div className="control">
+            <input
+              className="input"
+              type="password"
+              value={this.state.password}
+              onChange={e => this.setState({ password: e.target.value })}
+            />
+          </div>
+        </div>
+
         <br />
-        <p>パスワード</p>
-        <input
-          type="password"
-          value={this.state.password}
-          placeholder="********"
-          onChange={e => this.setState({ password: e.target.value })}
-        />
-        <br />
-        <button type="button" value="submit" onClick={this.handleLogin}>
-          新規登録
-        </button>
-        <button
-          onClick={() => {
-            this.props.history.push("/login");
-          }}
-        >
-          ログインはこちら
-        </button>
+
+        <div className="field is-grouped">
+          <div className="control">
+            <button className="button is-link" onClick={this.handleLogin}>
+              新規登録
+            </button>
+          </div>
+          <div className="control">
+            <button
+              className="button is-link is-light"
+              onClick={() => {
+                this.props.history.push("/login");
+              }}
+            >
+              ログインはこちら
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
