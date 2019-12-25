c81267a09b746e8a4463938ad338ab86d96c2f77
akakou <akakou571@gmail.com>
2019-12-25 23:34:52 +0900

    シーン追加フォームをいけいけにした

--- js/components/scene_form.js
+++ js/components/scene_form.js
@@ -20,33 +20,51 @@ class SceneForm extends React.Component {
     this.novel = await novelRepository.findById(this.prev.novelId);
 
     this.setState({
-      title: this.novel.title
+      title: this.novel.title,
+      prevContent: this.prev.content
     });
   }
 
   handlePost(e) {
     const sceneRepo = new SceneRepository();
-    sceneRepo.create({
+    const scene = sceneRepo.create({
       title: this.novel.title,
       content: this.state.content,
       prevId: this.prev.id,
       novelId: this.novel.id
     });
 
-    this.props.history.push("/");
+    this.props.history.push(`/#/scene/${scene.id}`);
   }
 
   render() {
     return (
       <div>
-        <h1>{this.state.title}</h1>
-        <textarea
-          onChange={e => this.setState({ content: e.target.value })}
-        ></textarea>
+        <h1 className="title">{this.state.title}</h1>
         <br />
-        <button type="button" value="submit" onClick={this.handlePost}>
-          投稿
-        </button>
+        <label>直前の内容</label>
+
+        <p>{this.state.prevContent}</p>
+        <br />
+        <div className="field">
+          <div className="control">
+            <label>追加するシーン</label>
+            <textarea
+              className="textarea"
+              placeholder="むかしむかしあるところにおじいさんとおばあさんがいました。"
+              onChange={e => this.setState({ content: e.target.value })}
+            ></textarea>
+          </div>
+        </div>
+        <br />
+
+        <div className="field is-grouped">
+          <div className="control">
+            <button className="button is-link" onClick={this.handlePost}>
+              投稿
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
