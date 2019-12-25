38f60d75f8694c7f7f5e9e67fad5c98d26df4e8d
akakou <akakou571@gmail.com>
2019-12-25 16:54:35 +0900

    小説リストを表示するページを作成

---
+++ js/components/novel_list.js
@@ -0,0 +1,48 @@
+class NovelList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handlePost = this.handlePost.bind(this);
+    this.state = {
+      novels: []
+    };
+  }
+
+  async componentDidMount() {
+    const novelRepository = new NovelRepository();
+    const novelId = getIdFromURI(this);
+    const novels = await novelRepository.getList(novelId);
+
+    this.setState({
+      novels: novels
+    });
+  }
+
+  handlePost(e) {}
+
+  render() {
+    var novels = [];
+
+    for (const novel of this.state.novels) {
+      const url = `/#/novel/${novel.id}`;
+
+      novels.push(
+        <div className="box" key={novel.id}>
+          <h2 className="title is-6">{novel.title}</h2>
+          <p>{novel.summary}</p>
+          <br></br>
+          <a className="button" href={url}>
+            この小説を読む
+          </a>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <h1 className="title">小説一覧</h1>
+        {novels}
+      </div>
+    );
+  }
+}
