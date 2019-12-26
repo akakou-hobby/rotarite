26936965e929effa679087f491cf94a94b2dc364
akakou <akakou571@gmail.com>
2019-12-26 12:40:09 +0900

    ログアウト機能を追加

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -6,6 +6,18 @@ class NavBar extends React.Component {
     this.state = {
       loggined: false
     };
+
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  async handleLogout(e) {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        console.log("ログアウトしました");
+        location.href = "/#/novels";
+      });
   }
 
   async componentDidMount() {
@@ -54,6 +66,19 @@ class NavBar extends React.Component {
                 <a className="navbar-item" href="https://twitter.com/_akakou">
                   開発者（akakou）
                 </a>
+                {this.state.loggined && (
+                  <div>
+                    <hr className="navbar-divider" />
+                    <a
+                      className="navbar-item"
+                      onClick={e => {
+                        this.handleLogout(e);
+                      }}
+                    >
+                      ログアウト
+                    </a>
+                  </div>
+                )}
               </div>
             </div>
 
