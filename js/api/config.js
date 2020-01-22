235fa2521b221b2b1847a0dc42d0e6a2b48743e6
akakou <akakou571@gmail.com>
2020-01-22 17:33:03 +0900

    コメントに引数を追加

--- js/api/config.js
+++ js/api/config.js
@@ -3,7 +3,21 @@
  * @author akakou
  */
 
+/**
+ * 設定
+ */
 const CONFIG = {
   BASE_URL: "/",
-  STATIC_URL: ""
+  STATIC_URL: "",
+
+  FIREBASE: {
+    apiKey: "AIzaSyDs6KIoUnAwCs1hDuIE-ZxxKZSQVMD09kw",
+    authDomain: "rotarite-f63f1.firebaseapp.com",
+    databaseURL: "https://rotarite-f63f1.firebaseio.com",
+    projectId: "rotarite-f63f1",
+    storageBucket: "rotarite-f63f1.appspot.com",
+    messagingSenderId: "1039269529152",
+    appId: "1:1039269529152:web:9b4eed34f5d42a461341a5",
+    measurementId: "G-J5E5G995V7"
+  }
 };
