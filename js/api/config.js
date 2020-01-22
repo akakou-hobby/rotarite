c0a379ea1dde6059af3604ee975a91c3fbc4a6d3
Kosei Akama <t19503ka@sfc.keio.ac.jp>
2020-01-22 17:45:16 +0900

    merge commit

--- js/api/config.js
+++ js/api/config.js
@@ -7,12 +7,8 @@
  * 設定
  */
 const CONFIG = {
-<<<<<<< HEAD
-    BASE_URL: "/~t19503ka/info1/final/final.html",
-    STATIC_URL: "/~t19503ka/info1/final/",
-=======
-  BASE_URL: "/",
-  STATIC_URL: "",
+  BASE_URL: "/~t19503ka/info1/final/final.html",
+  STATIC_URL: "/~t19503ka/info1/final/",
 
   FIREBASE: {
     apiKey: "AIzaSyDs6KIoUnAwCs1hDuIE-ZxxKZSQVMD09kw",
@@ -24,5 +20,4 @@ const CONFIG = {
     appId: "1:1039269529152:web:9b4eed34f5d42a461341a5",
     measurementId: "G-J5E5G995V7"
   }
->>>>>>> master
 };
