f35265bc558e0e4812fcf731e653b1f35040649c
akakou <akakou571@gmail.com>
2020-01-22 17:33:07 +0900

    コメントに引数を追加

--- js/api/firebase.js
+++ js/api/firebase.js
@@ -3,28 +3,5 @@
  * @author akakou
  */
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDs6KIoUnAwCs1hDuIE-ZxxKZSQVMD09kw",
-  authDomain: "rotarite-f63f1.firebaseapp.com",
-  databaseURL: "https://rotarite-f63f1.firebaseio.com",
-  projectId: "rotarite-f63f1",
-  storageBucket: "rotarite-f63f1.appspot.com",
-  messagingSenderId: "1039269529152",
-  appId: "1:1039269529152:web:9b4eed34f5d42a461341a5",
-  measurementId: "G-J5E5G995V7"
-};
-
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(CONFIG.FIREBASE);
 firebase.analytics();
-
-/**
- * 現在ログインしているユーザのuidを取得
- */
-const currentUser = () => {
-  try {
-    return firebase.auth().currentUser.uid;
-  } catch (e) {
-    console.log(e);
-    return null;
-  }
-};
