90ff3d161bd264d9a4209fefe518c7de662c520e
akakou <akakou571@gmail.com>
2020-01-22 17:54:18 +0900

    currentUserをもう一回足した

--- js/api/firebase.js
+++ js/api/firebase.js
@@ -5,3 +5,15 @@
 
 firebase.initializeApp(CONFIG.FIREBASE);
 firebase.analytics();
+
+/**
+ * 現在ログインしているユーザのuidを取得
+ */
+const currentUser = () => {
+  try {
+    return firebase.auth().currentUser.uid;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
